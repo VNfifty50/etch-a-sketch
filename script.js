edc9769f943a1870bd4427c3f9e78d1bddfcd9a8
VNfifty50 <145123015+VNfifty50@users.noreply.github.com>
2024-02-24 05:27:35 -0300

    button event listener added

--- script.js
+++ script.js
@@ -1,20 +1,70 @@
 let container = document.querySelector(".container");
 
-// create an element and append it to div
-// create another element and append it to the element before
+// triggers prompt which returns grid size 
+
+let btn = document.querySelector("#btn");
+let size = "16";
+let counter = 0;
 
 
+btn.addEventListener("click", () => {
+    size = prompt("The number of squares per side for the new grid?");
 
-for (let i = 0; i < 16; i++) {
-    let line = document.createElement("div");
-    line.classList.add("lines");
-    
-    for (let j = 0 ; j < 16; j++) {
-        let column = document.createElement("div");
-        column.textContent = `${i}${j}`;
-        column.classList.add("grid");
-        line.appendChild(column);
+    if (counter == 0) {
+        for (let i = 0; i < size; i++) {
+            let line = document.createElement("div");
+            line.classList.add("lines");
+            
+            for (let j = 0 ; j < size; j++) {
+                let column = document.createElement("div");
+                column.classList.add("grid");
+                line.appendChild(column);
+            }
+        
+            container.appendChild(line);
+        }
+
+    } else if (counter > 0) {          
+        // remove the grid before
+        for (let i = 0; i < size; i++) {
+                let line = document.querySelector(".lines");
+                container.removeChild(line);
+            
+            for (let j = 0 ; j < size; j++) {
+                let column = document.querySelector(".grid");
+                line.removeChild(column);
+            }
+        }
+        // create the new grid
+        for (let i = 0; i < size; i++) {
+            let line = document.createElement("div");
+            line.classList.add("lines");
+            
+            for (let j = 0 ; j < size; j++) {
+                let column = document.createElement("div");
+                column.classList.add("grid");
+                line.appendChild(column);
+                divs.push(column);
+            }
+        
+            container.appendChild(line);
+        }
     }
+    counter++;
+})
 
-    container.appendChild(line);
-}
+// Set up a hoover effect so the grid divs change color when mouse passes over them
+let grid = document.querySelector(".grid");
+
+container.addEventListener("mouseenter", (event) => {
+    // highlight the mouse enter target
+    event.target.style.backgroundColor = "";
+    event.target.style.backgroundColor = "red";
+})
+
+container.addEventListener("mouseout", function(event) {
+    event.target.style.backgroundColor = "white";
+})
+
+
+
